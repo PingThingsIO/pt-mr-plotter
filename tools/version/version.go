97581f2ae3fca1ef90ce69866b330fb9afae2b1c
Michael Andersen <m.andersen@cs.berkeley.edu>
2017-11-21 10:48:14 -0800

    4.7.0 tweaks to plotter

--- tools/version/version.go
+++ tools/version/version.go
@@ -1,4 +1,4 @@
-package version
+package main
 
 /*
 Given a version number MAJOR.MINOR.PATCH, increment the:
@@ -7,6 +7,6 @@ MAJOR version when you make incompatible API changes,
 MINOR version when you add functionality in a backwards-compatible manner, and
 PATCH version when you make backwards-compatible bug fixes.
 */
-const Major = 4
-const Minor = 6
-const Patch = 1
+const VersionMajor = 4
+const VersionMinor = 7
+const VersionPatch = 0
