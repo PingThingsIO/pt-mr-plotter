71798eb60dbe042870daff3b855d07e6b86deeac
Fredrik Antell <fred.antell@gmail.com>
2018-04-02 20:47:42 -0500

    Our version.go is different to allow for building on multiple OSes

--- tools/version/version.go
+++ tools/version/version.go
@@ -1,4 +1,4 @@
-package main
+package version
 
 /*
 Given a version number MAJOR.MINOR.PATCH, increment the:
@@ -7,6 +7,6 @@ MAJOR version when you make incompatible API changes,
 MINOR version when you add functionality in a backwards-compatible manner, and
 PATCH version when you make backwards-compatible bug fixes.
 */
-const VersionMajor = 4
-const VersionMinor = 9
-const VersionPatch = 5
+const Major = 4
+const Minor = 9
+const Patch = 5
