6e8b48860800820cf7e203717267ecb7a2695f73
Fredrik Antell <fred.antell@gmail.com>
2018-04-09 15:54:15 -0500

    Change docstring and simplify script for rebuilding docker image

--- container/rebuild.sh
+++ container/rebuild.sh
@@ -18,7 +18,20 @@ pushd ..
 
 # We run docker build from the root of the project which allows the Dockerfile to access all files
 # and no longer be restricted to just what exists in the ./container dir
-docker build -f ./container/Dockerfile-Rebuild --no-cache -t pingthings/mrplotter:${ver} .
-docker push pingthings/mrplotter:${ver}
-#docker tag pingthings/dev-mrplotter:${ver} pingthings/dev-mrplotter:latest
-#docker push pingthings/dev-mrplotter:latest
+
+##Choose how to Build the image
+
+#1) The Dockerfile-Rebuild uses the code in the current directory.
+docker build -f ./container/Dockerfile-Rebuild --no-cache -t predictivegrid/mr-plotter:${ver} .
+
+#2) The plain Dockerfile will clone from the PingThingsIO/pt-mr-plotter repo. You must have pushed any changes already and merged to master.
+#It must be run in the context of the container directory
+# popd && docker build -f ./Dockerfile --no-cache -t predictivegrid/mr-plotter:${ver} .
+
+
+#Uncomment any desired docker tags
+# docker tag predictivegrid/mr-plotter:${ver} localdev/mr-plotter:latest
+
+##Uncomment if you would like to push to dockerhub.
+# docker push predictivegrid/mr-plotter:${ver}
+
